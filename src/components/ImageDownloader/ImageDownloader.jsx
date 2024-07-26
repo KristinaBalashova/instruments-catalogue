3804adeb053b2e501012d9ac56ddfee7bd5ba3e4
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-07-26 14:07:27 +0200

    Add delete buttons and upd css

--- src/components/ImageDownloader/ImageDownloader.jsx
+++ src/components/ImageDownloader/ImageDownloader.jsx
@@ -1,32 +1,52 @@
 import { useState } from 'react';
 import styles from './ImageDownloader.module.css';
+import Button from '../Button/Button';
 
 const ImageDownloader = ({ setFile }) => {
   const [error, setError] = useState(null);
+  const [fileName, setFileName] = useState('');
+
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     const validTypes = ['image/jpeg', 'image/png'];
 
     if (file && validTypes.includes(file.type)) {
       setFile(file);
+      setFileName(file.name);
       setError('');
     } else {
       setError('Please upload a JPEG or PNG image.');
     }
   };
 
+  const handleDeleteFile = () => {
+    setFile(null);
+    setFileName('');
+    setError('');
+  };
+
   return (
     <div className={styles.root}>
-      <label>Download image:</label>
+      <label>Upload image:</label>
       <input
         type="file"
         accept="image/jpeg, image/png"
         onChange={handleFileChange}
         className={styles.input}
       />
-      <p className={styles.text}>
-        Download PNG or JPG picture, 500x500 max with removed background
-      </p>
+      <div className={styles.infoContainer}>
+        <p className={styles.text}>
+          Upload PNG or JPG picture, 500x500 max with removed background
+        </p>
+        {fileName && (
+          <div className={styles.fileInfo}>
+            <Button onClick={handleDeleteFile} secondary className={styles.deleteButton}>
+              Delete File
+            </Button>
+          </div>
+        )}
+      </div>
+
       {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
