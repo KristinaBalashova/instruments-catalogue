9e481581adb2e25a283fe902924e4a3b7f1354ac
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-07-26 14:08:41 +0200

    Add id for links to page

--- src/components/InstrumentCard/InstrumentCard.jsx
+++ src/components/InstrumentCard/InstrumentCard.jsx
@@ -1,28 +1,30 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import HeartSymbol from '../HeartSymbol/HeartSymbol';
 import styles from './InstrumentCard.module.css';
 import { UserContext } from '../../context/context';
 import EditorButtons from '../EditorButtons/EditorButtons';
+
 const InstrumentCard = ({ instrumentData }) => {
-  const { id, name, picture_url } = instrumentData;
+  const { id, name, image } = instrumentData;
   const { user } = useContext(UserContext);
 
   return (
     <>
       <div key={id} className={styles.cardContainer}>
-        <Link to="/instrument-page" state={{ instrumentData }} className={styles.link}>
-          <img src={picture_url} alt={name} className={styles.img} />
+        <Link to={`/instrument-page/${id}`} state={{ instrumentData }} className={styles.link}>
+          <img src={image} alt={name} className={styles.img} />
         </Link>
         <div className={styles.content}>
-          <Link to="/instrument-page" state={{ instrumentData }} className={styles.link}>
+          <Link to={`/instrument-page/${id}`} state={{ instrumentData }} className={styles.link}>
             <h3 className={styles.title}>{name}</h3>
           </Link>
           <div className={styles.buttons}>
-            {user === 'admin' && <EditorButtons />}
-            <div className={styles.fav}>
-              <HeartSymbol />
+            {user === 'admin' && <EditorButtons id={id} />}
+            {/*
+              <div className={styles.fav}>
+              <img src="/heart.svg" className={styles.heart} alt="favorite-heart" />
             </div>
+              */}
           </div>
         </div>
       </div>
