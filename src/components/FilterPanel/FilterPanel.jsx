08cc020be8f34497ee0c29d840352c6cb71590b1
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-08-11 11:14:52 +0200

    Filter panel now updates url with query parameters

--- src/components/FilterPanel/FilterPanel.jsx
+++ src/components/FilterPanel/FilterPanel.jsx
@@ -1,23 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styles from './FiltersPanel.module.css';
 import Button from '../Button/Button';
 
-const FiltersPanel = ({ setFilters, dataFilters }) => {
+const FiltersPanel = ({ dataFilters }) => {
+  const location = useLocation();
+  const navigateTo = useNavigate();
+
+  // Update the URL's searchParams when filters change
   const handleFilterChange = (event) => {
     const { name, value } = event.target;
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      [name]: value,
-    }));
+    const searchParams = new URLSearchParams(location.search);
+
+    if (value === '*') {
+      searchParams.delete(name); // Remove filter if it's set to "All"
+    } else {
+      searchParams.set(name, value); // Set the filter value in the URL
+    }
+
+    searchParams.set('page', 0); // Reset to the first page on filter change
+
+    navigateTo(`?${searchParams.toString()}`);
   };
 
+  // Clear all filters
   const handleFilterClear = () => {
-    setFilters((prevFilters) => ({
-      ...Object.keys(dataFilters).reduce((acc, filter) => {
-        acc[filter] = '*';
-        return acc;
-      }, {}),
-    }));
+    const searchParams = new URLSearchParams(location.search);
+
+    Object.keys(dataFilters).forEach((filter) => {
+      searchParams.delete(filter); // Remove all filters
+    });
+
+    searchParams.set('page', 0); // Reset to the first page
+
+    navigateTo(`?${searchParams.toString()}`);
   };
 
   return (
@@ -25,7 +41,13 @@ const FiltersPanel = ({ setFilters, dataFilters }) => {
       {Object.keys(dataFilters).map((filter) => (
         <form key={filter} className={styles.form}>
           <label className={styles.label}>{filter}</label>
-          <select id={filter} name={filter} onChange={handleFilterChange} className={styles.select}>
+          <select
+            id={filter}
+            name={filter}
+            onChange={handleFilterChange}
+            className={styles.select}
+            value={new URLSearchParams(location.search).get(filter) || '*'} // Set the current filter value from URL
+          >
             <option value={'*'}>{'All'}</option>
             {dataFilters[filter] &&
               dataFilters[filter].map((item) => (
