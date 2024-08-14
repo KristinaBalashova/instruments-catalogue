180aa335b3a87f3f7e4b6608f8bbd9b6b8edfbf4
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-08-14 14:43:48 +0200

    Add delete custom hook

---
+++ src/hooks/useDeleteItem.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { supabase } from '../supabaseClient';
+
+const useDeleteItem = () => {
+  const [statusDelete, setStatusDelete] = useState(null);
+  const [errorDelete, setErrorDelete] = useState(null);
+
+  const deleteItem = async (table, id, onSuccess) => {
+    const { error } = await supabase.from(table).delete().eq('id', id);
+
+    if (error) {
+      console.log(error);
+      setStatusDelete(false);
+      setErrorDelete('Error deleting data');
+    } else {
+      setStatusDelete(true);
+      if (onSuccess) onSuccess(id);
+
+      setTimeout(() => {
+        setStatusDelete(null);
+      }, 2000);
+    }
+  };
+
+  return {
+    deleteItem,
+    statusDelete,
+    errorDelete,
+  };
+};
+
+export default useDeleteItem;
