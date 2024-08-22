05cae29644e4e6675b42f6387aaf6551de92b4e2
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-08-22 13:52:09 +0200

    Add pagecounter, disable for pagination buttons, styles

--- src/components/PaginationButtons/PaginationButtons.jsx
+++ src/components/PaginationButtons/PaginationButtons.jsx
@@ -11,6 +11,8 @@ const PaginationButtons = ({
   setCurrentPage,
   isVisible,
 }) => {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
   const handleBackClick = () => {
     if (currentPage > 0) {
       setCurrentPage(currentPage - 1);
@@ -18,7 +20,7 @@ const PaginationButtons = ({
   };
 
   const handleForwardClick = () => {
-    if ((currentPage + 1) * itemsPerPage < totalItems) {
+    if (currentPage + 1 < totalPages) {
       setCurrentPage(currentPage + 1);
     }
   };
@@ -26,21 +28,29 @@ const PaginationButtons = ({
   if (!isVisible) return null;
 
   return (
-    <div className={styles.paginationButtons}>
-      <button
-        className={cx(styles.paginationButton, styles.back)}
-        onClick={handleBackClick}
-        disabled={currentPage === 0}
-      >
-        <IoIosArrowBack className={styles.arrow} />
-      </button>
-      <button
-        className={cx(styles.paginationButton, styles.forward)}
-        onClick={handleForwardClick}
-        disabled={(currentPage + 1) * itemsPerPage >= totalItems}
-      >
-        <IoIosArrowForward className={styles.arrow} />
-      </button>
+    <div className={styles.container}>
+      <span className={styles.pageCounter}>
+        Page {currentPage + 1} from {totalPages}
+      </span>
+
+      <div className={styles.buttons}>
+        <button
+          className={cx(styles.button, styles.back, { [styles.disabled]: currentPage === 0 })}
+          onClick={handleBackClick}
+          disabled={currentPage === 0}
+        >
+          <IoIosArrowBack className={styles.arrow} />
+        </button>
+        <button
+          className={cx(styles.button, styles.forward, {
+            [styles.disabled]: currentPage + 1 >= totalPages,
+          })}
+          onClick={handleForwardClick}
+          disabled={currentPage + 1 >= totalPages}
+        >
+          <IoIosArrowForward className={styles.arrow} />
+        </button>
+      </div>
     </div>
   );
 };
