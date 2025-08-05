cadb7da7993c3399294597324028c8bba0af77d7
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-08-05 17:17:23 +0200

    add: favories button to instrument info

--- src/components/InstrumentInfo/InstrumentInfo.jsx
+++ src/components/InstrumentInfo/InstrumentInfo.jsx
@@ -1,13 +1,19 @@
 import styles from './InstrumentInfo.module.css';
+import FavoriteButton from '../FavoriteButton/FavoriteButton';
 
 const InstrumentInfo = ({ data }) => {
   return (
     <div className={styles.root}>
-      <h2 className={styles.title}>{data.name}</h2>
+      <div className={styles.header}>
+        <h2 className={styles.title}>{data.name}</h2>
+        <FavoriteButton id={data.id} />
+      </div>
       <div className={styles.container}>
         {['brand', 'description', 'country', 'materials', 'type', 'date'].map((item) => (
           <p key={item} className={styles.item}>
-            <span className={styles.name}>{item.charAt(0).toUpperCase() + item.slice(1)}: </span>
+            <span className={styles.name}>
+              {item.charAt(0).toUpperCase() + item.slice(1)}:
+            </span>
             {data[item]}
           </p>
         ))}
