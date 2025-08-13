97d3682b4e37885b2e035f9563333d6eb9d47389
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-08-13 18:07:50 +0200

    fix: import for hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,8 +1,25 @@
-export { default as useDeleteItem } from './useDeleteItem';
-export { default as useFetchItem } from './useFetchItem';
-export { default as useUploadImage } from './useUploadImage';
-export { default as useSignIn } from './useSignIn';
-export { default as useSignUp } from './useSignUp';
-export { default as useSignOut } from './useSignOut';
-export { default as useResendConfirmation } from './useResendConfirmation';
-export { default as useFetchFilteredData } from './useFetchFilteredData';
+import useDeleteItem from './useDeleteItem';
+import useFetchItem from './useFetchItem';
+import useUploadImage from './useUploadImage';
+import useSignIn from './useSignIn';
+import useSignOut from './useSignOut';
+import useSignUp from './useSignUp';
+import useResendConfirmation from './useResendConfirmation';
+import useFetchFilteredData from './useFetchFilteredData';
+//import useInstrumentFilters from './useInstrumentFilters';
+//import useInstrumentsData from './useInstrumentsData';
+//import useInstrumentFilterOptions from './useInstrumentFilterOptions';
+
+export {
+  useDeleteItem,
+  useFetchItem,
+  useUploadImage,
+  useSignIn,
+  useSignOut,
+  useSignUp,
+  useResendConfirmation,
+  useFetchFilteredData,
+  //useInstrumentFilters,
+  //useInstrumentsData,
+  //useInstrumentFilterOptions,
+};
