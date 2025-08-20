2f06bb8f639e28075e8e714872b699450bd05bab
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-08-20 18:58:43 +0200

    add: tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from './store';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-     <App />
-    </React.StrictMode>
-  </Provider>,
+const queryClient = new QueryClient();
 
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
 );
+
