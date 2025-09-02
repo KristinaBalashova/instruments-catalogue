b119ea2e966c1de4c2f8975f3fa04050001b5e82
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-09-02 19:24:35 +0200

    fix: theme saved to lokal store

--- src/store/helpers/themeMiddleware.js
+++ src/store/helpers/themeMiddleware.js
@@ -3,7 +3,7 @@ export const themeMiddleware = (store) => (next) => (action) => {
 
   if (action.type === 'theme/toggleTheme') {
     const theme = store.getState().theme.value;
-    localStorage.setItem('theme', theme);
+    localStorage.setItem('theme',  JSON.stringify(theme));
   }
 
   return result;
